480b1bdefed743654630cdcdc39259b8a2cec5ed
David Jelenc <david.jelenc@fri.uni-lj.si>
2020-11-19 23:58:28 +0100

    Updates project

--- app/src/main/java/ep/appobject/HelpActivity.kt
+++ app/src/main/java/ep/appobject/HelpActivity.kt
@@ -1,7 +1,8 @@
 package ep.appobject
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_help.*
 
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
